edf5fe8a2c8175965b7ae54d6365e7e9ba90063c
Lauren Posey and Meaghan Jones <lauren+meaghan@>
2016-07-18 13:08:41 -0700

    do things i thinl

--- app.rb
+++ app.rb
@@ -21,8 +21,23 @@ end
 
 post('/divisions') do
   name = params.fetch("name")
-
   division = Division.create({:name => name, :id => nil})
   @divisions = Division.all()
   erb(:divisions)
+end
+
+get('/employees') do
+  @employees = Employee.all()
+  erb(:employees)
+end
+
+get("/employees/new")do
+  erb(:employee_form)
+end
+
+post('/employees') do
+  name = params.fetch("name")
+  employee = Employee.create({:name => name, :id => nil})
+  @employees = Employee.all()
+  erb(:employees)
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,3 +12,13 @@ describe('adding a new division', {:type => :feature}) do
     expect(page).to have_content('HR')
   end
 end
+
+describe('adding a new employee', {:type => :feature}) do
+  it('allows a user to add an employee to a division') do
+    visit('/')
+    click_link('Add new employee')
+    fill_in('name', :with =>'Jane')
+    click_button('Add employee')
+    expect(page).to have_content('Jane')
+  end
+end
