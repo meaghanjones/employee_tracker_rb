d60b20b89b2a0ee1f398e71087ff6addeb94ec51
Lauren Posey and Meaghan Jones <meaghan@>
2016-07-18 16:00:57 -0700

    add passing integration specs allows a user to update the name of a division, allows a user to delete a division, and allows a user to delete an employee

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -23,6 +23,32 @@ describe('adding a new division', {:type => :feature}) do
   end
 end
 
+describe('update a division', {:type => :feature}) do
+  it('allows a user to update the name of a division') do
+    visit('/')
+    click_link('Add new division')
+    fill_in('name', :with =>'HR')
+    click_button('Add division')
+    click_link('HR')
+    click_link('Update')
+    fill_in('name', :with => "Legal")
+    click_button('Update')
+    expect(page).to have_content("Legal")
+  end
+
+  it('allows a user to delete a division') do
+    visit('/')
+    click_link('Add new division')
+    fill_in('name', :with =>'HR')
+    click_button('Add division')
+    click_link('HR')
+    click_link('Update')
+    click_button('Delete')
+    expect(page).to have_content("Current Divisions")
+  end
+end
+
+
 describe('adding a new employee', {:type => :feature}) do
   it('allows a user to add an employee to a division') do
     visit('/')
@@ -50,5 +76,19 @@ describe('adding a new employee', {:type => :feature}) do
     fill_in('name', :with => 'Janet')
     click_button('Update')
     expect(page).to have_content('Janet')
+  end
+
+  it('allows a user to delete an employee')do
+    visit('/')
+    click_link('Add new division')
+    fill_in('name', :with =>'HR')
+    click_button('Add division')
+    visit('/')
+    click_link('Add new employee')
+    fill_in('name', :with =>'Jane')
+    click_button('Add employee')
+    click_link('Jane')
+    click_button('Delete')
+    expect(page).to have_content('Current Employees')
   end
 end
